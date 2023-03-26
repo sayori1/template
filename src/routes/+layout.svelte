df48dafcd3850a9451c60a1b728018a8a504207a
sayori1 <ridickeon@yandex.com>
2023-03-26 11:01:39 +0500

    add test bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
     export const ssr = false;
 	import './style.css';
     import Notifications from 'svelte-notifications';
+  import NavigationBar from './widgets/NavigationBar.svelte';
 </script>
 
+<NavigationBar></NavigationBar>
 <Notifications>
     <main style="width:70%">
         <slot />
---
+++ src/routes/widgets/NavigationBar.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { onMount } from 'svelte';
+  import Row from '../../widgets/Row.svelte';
+  import SizedBox from '../../widgets/SizedBox.svelte';
+	let mounted = false;
+	onMount(() => {
+		mounted = true;
+	});
+</script>
+
+<div
+	style="background-color:#fbfbfa; position: fixed; top:0px; width: 100%; border-bottom: 1px solid #f5f5f4;"
+>
+	<Row crossAxisAlignment="center">
+		<SizedBox width="1vw" />
+		<h2 style="margin: 10px; font-size: 3vh">academy</h2>
+		<SizedBox width="4vw" />
+		<a href="/journal">обзор</a>
+		<a href="/me">профиль</a>
+	</Row>
+</div>
+
+<style>
+	a {
+		cursor: pointer;
+		margin-right: 3%;
+	}
+	a:hover {
+		color: rgb(71, 71, 71);
+	}
+	a {
+		background-color: transparent;
+		text-decoration: none;
+	}
+	a:hover {
+		text-decoration: none;
+	}
+</style>
